98f8f6111e1585d8f6e2965aa165094ab4ec297c
Emily Wirtz <ewirtz3@gmail.com>
2020-08-02 17:26:33 -0500

    finished contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,22 @@
 import React from "react";
-import { Button, Divider, Form } from "semantic-ui-react";
+import { Button, Form, Container, TextArea, Segment } from "semantic-ui-react";
+import "../assets/css/Contact.css";
 
 export default function Contact() {
-  return <p>contact</p>;
+  return (
+    <Container>
+      <Segment centered raised id="contact-segment">
+        <Form size="big">
+          <Form.Input label="name" control="input" placeholder="Your name" />
+          <Form.Input label="email" control="input" placeholder="Your email" />
+          <Form.Field
+            control={TextArea}
+            label="message"
+            placeholder="Your message"
+          />
+          <Form.Field control={Button} content="Submit" basic color="yellow" />
+        </Form>
+      </Segment>
+    </Container>
+  );
 }
