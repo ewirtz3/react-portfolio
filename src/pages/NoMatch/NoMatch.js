b86e63ede0d0fc85e91931b2170250cd750f181b
Emily Wirtz <ewirtz3@gmail.com>
2020-08-05 22:57:13 -0500

    finishing noMatch and readme

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Container, Segment } from "semantic-ui-react";
+import { Container, Segment, Icon, Header } from "semantic-ui-react";
+import "./NoMatch.css";
 
 export default function NoMatch() {
   return (
-    <Container>
-      <Segment />
+    <Container className="noMatch">
+      <Segment raised textAlign="center" centered="true">
+        <Icon name="angle double left" size="huge" color="yellow" />
+        <Icon name="angle double left" size="huge" color="yellow" />
+        <Icon name="angle double left" size="huge" color="yellow" />
+        <Icon name="angle double left" size="huge" color="yellow" />
+
+        <p as="h2" color="yellow" size="huge" className="oops">
+          Oops! Let's get you back{" "}
+          <a href={process.env.PUBLIC_URL + "/home"}>home</a>
+        </p>
+      </Segment>
     </Container>
   );
 }
