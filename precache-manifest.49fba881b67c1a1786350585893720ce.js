e0ffde9d663840bd470b9f0b7cd062ec0d06cdc1
Emily Wirtz <ewirtz3@gmail.com>
2020-08-06 18:03:16 -0500

    Updates

---
+++ precache-manifest.49fba881b67c1a1786350585893720ce.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b904277cf60bcc72cfff6e1ae7d72656",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "200d82b78458314b79bf",
+    "url": "/react-portfolio/static/css/2.ffb0573e.chunk.css"
+  },
+  {
+    "revision": "9fe2881b0b1d13ad9f78",
+    "url": "/react-portfolio/static/css/main.742c776f.chunk.css"
+  },
+  {
+    "revision": "200d82b78458314b79bf",
+    "url": "/react-portfolio/static/js/2.fe9c9f01.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/react-portfolio/static/js/2.fe9c9f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe2881b0b1d13ad9f78",
+    "url": "/react-portfolio/static/js/main.66b243e4.chunk.js"
+  },
+  {
+    "revision": "7ab49a546a33138f93d9",
+    "url": "/react-portfolio/static/js/runtime-main.bc6ed5e9.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "beed5e5e5e1a1f29f24e24190275eaff",
+    "url": "/react-portfolio/static/media/burger.beed5e5e.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "39628664db975fd886debceeeb6a4048",
+    "url": "/react-portfolio/static/media/inOrOut (1).39628664.jpg"
+  },
+  {
+    "revision": "db8772550b34abe4ee15335661a0e7d4",
+    "url": "/react-portfolio/static/media/noteTaker.db877255.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "77b66bf8f9ca51828460b7585d85475b",
+    "url": "/react-portfolio/static/media/profilePic.77b66bf8.jpg"
+  },
+  {
+    "revision": "9b63f0148ae897239cde4726824d4b0f",
+    "url": "/react-portfolio/static/media/projectYou.9b63f014.jpg"
+  },
+  {
+    "revision": "07be563c3504b484bd4fad00f1d3f958",
+    "url": "/react-portfolio/static/media/resume.07be563c.pdf"
+  },
+  {
+    "revision": "b00e539fb07927077ea75d8572b8db10",
+    "url": "/react-portfolio/static/media/spain5.b00e539f.jpg"
+  },
+  {
+    "revision": "d0766bc914ac5a51a2bb3c6c0e69badd",
+    "url": "/react-portfolio/static/media/userDirectory.d0766bc9.jpg"
+  },
+  {
+    "revision": "6220f9f0e4b3a2996d97e2f078efd95a",
+    "url": "/react-portfolio/static/media/weatherDashPic.6220f9f0.jpg"
+  }
+]);
